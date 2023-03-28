12bd0ea0e51f4e4ba1177479a6b539e44708fc52
TinaByrd <thuaneoliveira1@gmail.com>
2023-03-28 11:40:47 -0300

    polimorfismo e sobrescrita

---
+++ poo-polimorfismo/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ClasseMae[] classes = new ClasseMae[] {new ClasseFilha1(), new ClasseFilha2(), new ClasseMae()};
+
+        for (ClasseMae classe: classes) {
+            classe.metodo1();
+        }
+
+        for (ClasseMae classe: classes) {
+            classe.metodo2();
+        }
+
+        System.out.println("");
+
+        ClasseFilha2 classeFilha2 = new ClasseFilha2();
+        classeFilha2.metodo2(); //sobrescrita. Aqui não há polimorfismo
+    }
+}
